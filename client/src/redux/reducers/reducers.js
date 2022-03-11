59580aff1c29b561d873ee066251f52e900bcd91
EzeTP <tejada.eap@gmail.com>
2022-03-11 00:24:32 -0300

    starting with the boilerplate from the front

---
+++ client/src/redux/reducers/reducers.js
@@ -0,0 +1,11 @@
+const initialState = {
+  products: [],
+  productDetail: {},
+};
+
+export default function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
